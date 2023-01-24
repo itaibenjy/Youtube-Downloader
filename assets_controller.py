7907bb19264e79481fbea11925f9eeea0a2bff26
itai <itai.benjamin@e.braude.ac.il>
2023-01-24 15:06:23 +0200

    changing to icons

---
+++ assets_controller.py
@@ -0,0 +1,23 @@
+import customtkinter
+from PIL import Image
+import os
+
+class NavigatorAssets():
+
+    def __init__(self) -> None:
+        # path to the images directory
+        self.image_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "assets" , "images")
+
+        # icon dimensions
+        self.icon_dimensions:tuple = (45,45)
+
+        # initialize all the images
+        #self.logo_image = customtkinter.CTkImage(Image.open(os.path.join(self.image_path, "CustomTkinter_logo_single.png")), size=(26, 26))
+        self.search_icon = customtkinter.CTkImage(light_image=Image.open(os.path.join(self.image_path, "search-light.png")),
+                                                 dark_image=Image.open(os.path.join(self.image_path, "search-dark.png")), size=self.icon_dimensions)
+        self.url_icon = customtkinter.CTkImage(light_image=Image.open(os.path.join(self.image_path, "url-light.png")),
+                                                 dark_image=Image.open(os.path.join(self.image_path, "url-dark.png")), size=self.icon_dimensions)
+        self.download_icon = customtkinter.CTkImage(light_image=Image.open(os.path.join(self.image_path, "download-light.png")),
+                                                 dark_image=Image.open(os.path.join(self.image_path, "download-dark.png")), size=self.icon_dimensions)
+        self.settings_icon = customtkinter.CTkImage(light_image=Image.open(os.path.join(self.image_path, "settings-light.png")),
+                                                     dark_image=Image.open(os.path.join(self.image_path, "settings-dark.png")), size=self.icon_dimensions)
--- nav_frame.py
+++ nav_frame.py
@@ -1,5 +1,6 @@
 import customtkinter
 from enum import Enum
+from assets_controller import NavigatorAssets
 
 class Frame(Enum):
     SEARCH = 1
@@ -7,16 +8,20 @@ class Frame(Enum):
     DOWNLOADING = 3
     SETTINGS = 4
 
-HOVER_COLOR = ("gray70", "gray30");
-TEXT_COLOR = ("gray10", "gray90");
-SELECT_COLOR = ("gray75","gray25");
+HOVER_COLOR:tuple = ("gray70", "gray30");
+TEXT_COLOR:tuple = ("gray10", "gray90");
+SELECT_COLOR:tuple = ("gray75","gray25");
 
-class NavigatorFrame:
+BUTTON_HEIGHT:int = 55;
+
+class NavigatorFrame():
 
     def __init__(self, app) -> None:
 
+        # getting access to all assets
+        self.assets = NavigatorAssets()
         # setting the frame
-        self.frame = customtkinter.CTkFrame(app, width=200, corner_radius=0)
+        self.frame = customtkinter.CTkFrame(app, width=20, corner_radius=0)
         self.frame.grid(row=0, column=0, rowspan= 4, sticky="nsew")
 
         # configure 6X1
@@ -24,38 +29,39 @@ class NavigatorFrame:
         self.frame.grid_rowconfigure(5,  weight=3)
 
         # Title - Youtube Downloader
-        self.title = customtkinter.CTkLabel(self.frame, text="Youtube Downloader", font=customtkinter.CTkFont(size=20, weight="bold"))
+        self.title = customtkinter.CTkLabel(self.frame, text="YD", font=customtkinter.CTkFont(size=30, weight="bold"))
         self.title.grid(row=0, column=0, padx=20, pady=40)
 
         # Navigation Buttons 
-        self.search = customtkinter.CTkButton(self.frame, corner_radius=0, height=40, border_spacing=10, text="Search",
+        self.search = customtkinter.CTkButton(self.frame, corner_radius=0, height=BUTTON_HEIGHT, border_spacing=10, text="",
                                               fg_color="transparent", text_color=TEXT_COLOR, hover_color=HOVER_COLOR,
-                                              command=self.search_button_event)
+                                              image=self.assets.search_icon,command=self.search_button_event)
         self.search.grid(row=2, column=0, sticky="ew")
 
-        self.url = customtkinter.CTkButton(self.frame, corner_radius=0, height=40, border_spacing=10, text="By URL",
+        self.url = customtkinter.CTkButton(self.frame, corner_radius=0, height=BUTTON_HEIGHT, border_spacing=10, text="",
                                               fg_color="transparent", text_color=TEXT_COLOR, hover_color=HOVER_COLOR,
-                                              command=self.url_button_event)
+                                              image=self.assets.url_icon,command=self.url_button_event)
         self.url.grid(row=3, column=0, sticky="ew")
 
-        self.downloading = customtkinter.CTkButton(self.frame, corner_radius=0, height=40, border_spacing=10, text="Downloading",
+        self.downloading = customtkinter.CTkButton(self.frame, corner_radius=0, height=BUTTON_HEIGHT, border_spacing=10, text="",
                                               fg_color="transparent", text_color=TEXT_COLOR, hover_color=HOVER_COLOR,
-                                              command=self.downloading_button_event)
+                                              image=self.assets.download_icon,command=self.downloading_button_event)
         self.downloading.grid(row=4, column=0, sticky="ew")
 
-        self.settings = customtkinter.CTkButton(self.frame, corner_radius=0, height=40, border_spacing=10, text="Settings",
+        self.settings = customtkinter.CTkButton(self.frame, corner_radius=0, height=BUTTON_HEIGHT, border_spacing=10, text="",
                                               fg_color="transparent", text_color=TEXT_COLOR, hover_color=HOVER_COLOR,
-                                              command=self.settings_button_event)
+                                              image=self.assets.settings_icon,command=self.settings_button_event)
         self.settings.grid(row=6, column=0, sticky="sew")
 
 
-    def select_frame(self, frame: Frame):
-        # transparent 
+    def select_frame(self, frame: Frame) -> None:
+        # make all the buttons transparent
         self.search.configure(fg_color="transparent")
         self.url.configure(fg_color="transparent")
         self.downloading.configure(fg_color="transparent")
         self.settings.configure(fg_color="transparent")
 
+        # update which button was selected
         match frame:
             case Frame.SEARCH:
                 self.search.configure(fg_color=SELECT_COLOR)
